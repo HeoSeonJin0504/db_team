1323219fb9aec47c04f0f7725880abcc0d645eb2
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2024-12-05 20:06:45 +0900

    기능 구현 (1차)

--- db_teamproject/src/pages/Home.tsx
+++ db_teamproject/src/pages/Home.tsx
@@ -1,58 +1,88 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Modal from "react-modal";
 
-// 컨테이너 스타일 정의
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 60px;
-  height: 100vh;
+  padding: 20px;
 `;
 
-// 본문 내용을 감싸는 폼
-const Form = styled.div`
+const LeftContainer = styled.div`
+  flex: 1;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-between;
+  padding: 40px;
+`;
+
+const RightContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
+
+const LeftForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   padding: 20px;
   border: 2px solid black;
   border-radius: 10px;
-  width: 50vw;
-  height: 50vh;
+  width: 40vw;
+  height: 40vh;
+  text-align: center;
+  justify-content: center;
+  margin-top: 120px;
+  background-color: white;
 `;
 
-// 기본 파일 선택 스타일 숨기기
+const RightForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  border: 2px solid black;
+  border-radius: 10px;
+  width: 40vw;
+  min-height: 40vh;
+  max-height: 100vh;
+  text-align: center;
+  justify-content: center;
+  margin-top: 120px;
+  background-color: white;
+`;
+
 const FileInput = styled.input`
   display: none;
 `;
 
-// 버튼 컨테이너
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 20px; /* 버튼 사이의 간격 추가 */
+  gap: 20px;
   margin-top: 20px;
 `;
 
-// 버튼
 const Button = styled.button`
   padding: 15px;
   text-decoration: none;
-  border-radius: 5px;
-  border: 1px solid black;
-  color: black;
+  background: #abb7b7;
+  border: 2px solid #abb7b7;
+  font-weight: bold;
+  border-radius: 7px;
+  color: white;
   font-size: 1.5em;
-  background-color: white;
   cursor: pointer;
 
   &:hover {
   }
 `;
 
-// 이미지 미리보기 스타일 정의
 const ImagePreview = styled.img`
   max-width: 100%;
   max-height: 100%;
@@ -61,12 +91,58 @@ const ImagePreview = styled.img`
   border-radius: 5px;
 `;
 
-const GetStarted = () => {
-  // 선택된 이미지 파일과 미리보기 URL을 저장할 상태 변수
+const ListBox = styled.ul`
+  width: 100%;
+  height: 200px;
+  margin-top: 20px;
+  list-style-type: none;
+  padding: 0;
+  overflow-y: auto;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const ListItem = styled.li<{ selected: boolean }>`
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+  background-color: ${(props) => (props.selected ? "#e0e0e0" : "white")};
+  cursor: pointer;
+
+  &:last-child {
+    border-bottom: 2px solid #ccc; /* 마지막 행을 두껍게 */
+  }
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+const ModalImage = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const ModalButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+const Home = () => {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [imageList, setImageList] = useState<{ name: string; path: string }[]>([]);
+  const [selectedImagePath, setSelectedImagePath] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // 파일 선택 시 호출되는 함수
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
@@ -81,48 +157,104 @@ const GetStarted = () => {
 
   const handleAnalyzeClick = async () => {
     if (!selectedImage) {
-      alert('이미지를 먼저 업로드 해주세요.');
+      alert("이미지를 먼저 업로드 해주세요.");
       return;
     }
 
     const formData = new FormData();
-    formData.append('file', selectedImage as Blob);
+    formData.append("file", selectedImage as Blob);
 
     try {
-      const response = await fetch('http://localhost:8000/image-save', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8000/image-save", {
+        method: "POST",
         body: formData,
       });
       const result = await response.json();
-      console.log('Response data:', result);
-      alert(result);
+      console.log("Response data:", result);
+      alert(result.message);
     } catch (error) {
-      console.error('Error:', error);
-      alert('이미지 저장에 실패했습니다.(서버 오류)');
+      console.error("Error:", error);
+      alert("이미지 저장 실패(서버 오류)");
     }
+  };
+
+  const fetchImages = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/images");
+      const data = await response.json(); // 이미지 목록 가져오는 코드
+      setImageList(data);
+    } catch (error) {
+      console.error("Error fetching images:", error);
+      alert("이미지 새로고침 실패 (서버 오류)");
+    }
+  };
+
+  const handleImageClick = (path: string) => {
+    setSelectedImagePath(path);
+  };
+
+  const handleViewImageClick = () => {
+    if (selectedImagePath) {
+      setIsModalOpen(true);
+    } else {
+      alert("이미지를 선택해주세요.");
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
     <Container>
-      <Form>
-        {imagePreview && (
-          <ImagePreview src={imagePreview} alt="Selected file preview" />
-        )}
-      </Form>
-      <ButtonContainer>
-        <label htmlFor="fileInput" aria-label="이미지 선택">
-          <Button as="span">이미지 열기</Button>
-        </label>
-        <FileInput
-          id="fileInput"
-          type="file"
-          accept="image/*"
-          onChange={handleFileChange}
-        />
-        <Button onClick={handleAnalyzeClick}>이미지 저장</Button>
-      </ButtonContainer>
+      <LeftContainer>
+        <LeftForm>
+          {imagePreview && (
+            <ImagePreview src={imagePreview} alt="Selected file preview" />
+          )}
+        </LeftForm>
+        <ButtonContainer>
+          <label htmlFor="fileInput" aria-label="이미지 선택">
+            <Button as="span">이미지 열기</Button>
+          </label>
+          <FileInput
+            id="fileInput"
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+          <Button onClick={handleAnalyzeClick}>이미지 저장</Button>
+        </ButtonContainer>
+      </LeftContainer>
+      <RightContainer>
+        <RightForm>
+          <ListBox>
+            {imageList.map((image, index) => (
+              <ListItem
+                key={index}
+                selected={selectedImagePath === image.path}
+                onClick={() => handleImageClick(image.path)}
+              >
+                {`${image.name} - ${image.path}`}
+              </ListItem>
+            ))}
+          </ListBox>
+        </RightForm>
+        <ButtonContainer>
+          <Button onClick={fetchImages}>새로고침</Button>
+          <Button onClick={handleViewImageClick}>이미지 보기</Button>
+        </ButtonContainer>
+      </RightContainer>
+      <Modal isOpen={isModalOpen} onRequestClose={closeModal} contentLabel="Image Modal">
+        <ModalContent>
+          <ModalImage src={selectedImagePath || ""} alt="Selected Image" />
+          <ModalButtonContainer>
+            <Button onClick={closeModal}>닫기</Button>
+          </ModalButtonContainer>
+        </ModalContent>
+      </Modal>
     </Container>
   );
 };
 
-export default GetStarted;
+export default Home;
